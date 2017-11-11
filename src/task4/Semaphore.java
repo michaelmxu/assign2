73ea62fc0c12c51f8cde15d871055e41daeeda64
michaelmxu <32373152+michaelmxu@users.noreply.github.com>
2017-11-10 21:42:06 -0500

    Update Semaphore.java

--- src/task4/Semaphore.java
+++ src/task4/Semaphore.java
@@ -1,47 +1,46 @@
-package task4;
-public class Semaphore 
-{
-	private int value;
-	public Semaphore(int value)
-	{
-		this.value = value;
-	}
-	
-	public Semaphore()
-	{
-		this(0);
-	}
-	
-	public synchronized void Wait()
-	{
-		while (this.value <= 0)
-		{
-			try
-			{
-				wait();
-			}
-			catch(InterruptedException e)
-			{
-				System.out.println ("Semaphore::Wait() - caught InterruptedException: " + e.getMessage() );
-				e.printStackTrace();
-			}
-		}
-		this.value--;    
-	}
-	
-	public synchronized void Signal()
-	{
-		++this.value;
-		notify();
-	}
-	
-	public synchronized void P()
-	{
-		this.Wait();
-	}
-	
-	public synchronized void V()
-	{
-		this.Signal();
-	}
+package task2;
+
+public class Semaphore {
+	   private int value;
+       public Semaphore(int value)
+       {
+                this.value = value;
+       }
+      public Semaphore()
+      {
+               this(0);
+       }
+      
+      public synchronized void Wait()
+      {
+                while (this.value <= 0)
+                {
+                       try
+                      {
+                    	   this.value--;
+                           wait();
+                           this.value++;
+                       }
+                      catch(InterruptedException e)
+                      {
+                               System.out.println ("Semaphore::Wait() - caught InterruptedException: " + e.getMessage() );
+                               e.printStackTrace();
+                          }
+                  }
+                  this.value--;    
+         }
+         public synchronized void Signal()
+         {
+                 ++this.value;
+                 notify();
+         }
+         public synchronized void P()
+         {
+                 this.Wait();
+         }
+        public synchronized void V()
+        {
+                 this.Signal();
+        }
+
 }
