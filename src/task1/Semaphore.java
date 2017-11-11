b4a756bd99b0b1efd10d9632f6b941702d99651d
michaelmxu <32373152+michaelmxu@users.noreply.github.com>
2017-11-10 21:41:25 -0500

    Update Semaphore.java

--- src/task1/Semaphore.java
+++ src/task1/Semaphore.java
@@ -1,48 +1,46 @@
-// Source code for semaphore class:
 package task1;
-class Semaphore 
-{
-         private int value;
-         public Semaphore(int value)
+
+public class Semaphore {
+	   private int value;
+       public Semaphore(int value)
+       {
+                this.value = value;
+       }
+      public Semaphore()
+      {
+               this(0);
+       }
+      
+      public synchronized void Wait()
+      {
+                while (this.value <= 0)
+                {
+                       try
+                      {
+                    	   this.value--;
+                           wait();
+                           this.value++;
+                       }
+                      catch(InterruptedException e)
+                      {
+                               System.out.println ("Semaphore::Wait() - caught InterruptedException: " + e.getMessage() );
+                               e.printStackTrace();
+                          }
+                  }
+                  this.value--;    
+         }
+         public synchronized void Signal()
          {
-                  this.value = value;
+                 ++this.value;
+                 notify();
          }
-        public Semaphore()
-        {
-                 this(0);
+         public synchronized void P()
+         {
+                 this.Wait();
          }
-        public synchronized void Wait()
+        public synchronized void V()
         {
-                  while (this.value <= 0)
-                  {
-                         try
-                        {
-                        	 this.value--;
-                             wait();
-                             this.value++;
-                         }
-                        catch(InterruptedException e)
-                        {
-                                 System.out.println ("Semaphore::Wait() - caught InterruptedException: " + e.getMessage() );
-                                 e.printStackTrace();
-                            }
-                    }
-                    this.value--;    
-           }
-           public synchronized void Signal()
-           {
-                   ++this.value;
-                   notify();
-           }
-           public synchronized void P()
-           {
-                   this.Wait();
-           }
-          public synchronized void V()
-          {
-                   this.Signal();
-          }
+                 this.Signal();
+        }
+
 }
-
-// Source code for character stack:
-
